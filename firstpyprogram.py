4301a2708024e9f7229366212180f03ee69add4e
B.Pranay Kumar <pranay.b9106@gmail.com>
2025-03-11 13:25:01 +0530

    Add files via upload

---
+++ firstpyprogram.py
@@ -0,0 +1,11 @@
+x = 10
+y = 15
+list = [10,20,30,40,50]
+if (x not in list):
+  print("x is not present in the class")
+else:
+  print("x is present in the class")
+if(y in list):
+  print("y is present in the class")
+else:
+  print("y is not present in the class")   
